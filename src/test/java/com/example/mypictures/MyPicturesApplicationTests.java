9aa4eee1e3970c0c5a113576e4cfe72ff7b85c3f
bmana21 <bmana21@freeuni.edu.ge>
2023-08-27 16:42:27 +0400

    Initial commit

---
+++ src/test/java/com/example/mypictures/MyPicturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mypictures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPicturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
